02c8a4d22726eb84cbc4cf4fe0a9f896e849f990
Gab Fortin <gabfortin@icloud.com>
2020-09-24 21:47:54 -0400

    Reorganized files

---
+++ MailError.py
@@ -0,0 +1,85 @@
+from tkinter import Tk, Label, Button, messagebox
+import os
+import re
+import pip
+
+def install(package):
+	if hasattr(pip, 'main'):
+		pip.main(['install', package])
+	else:
+		pip._internal.main(['install', package])
+
+try:
+	import extract_msg
+except:
+    try:
+        install('extract-msg')
+        import extract_msg
+    except:
+        raise Exception("Nous n'avons pas pu installer extract-msg. S'il vous plait l'installer manuellement.")
+
+class ErreurMail:
+	def __init__(self, master):
+		self.master = master
+		master.title("ErreurMail")
+
+		self.title = Label(master, text="Bienvenue dans ErreurMail")		
+		self.title.pack()
+		
+		self.label = Label(master, text="Mettre les fichiers .msg à lire dans le dossier Emails")
+		self.label.pack()
+
+		self.label2 = Label(master, text="Appuyer sur le bouton pour lire les fichiers")
+		self.label2.pack()
+		
+		self.greet_button = Button(master, text="Obtenir les adresses", command=self.parse)
+		self.greet_button.pack()
+
+	def parse(self):		
+		fichiers = os.listdir('./Emails')
+		
+		bad_addresses = []
+		
+		for fichier in fichiers:
+			
+			if '.msg' in fichier:
+				msg = extract_msg.Message(fichier)
+				msg_message = msg.body
+				
+				if re.findall('Failed+\s+Recipient+:+.+', msg_message):
+					address = re.findall('Failed+\s+Recipient+:+.+', msg_message)[0]
+					address = address.replace('Failed Recipient: ', '')
+
+					if '<' in address:
+						address = address.replace('<', '')
+				
+				elif re.findall('\(+.+\@+.+\)', msg_message):
+					address = re.findall('\(+.+\@+.+\)', msg_message)[0]
+					address = address.replace('(', '').replace(')', '')
+				
+				else:
+					print("Le fichier nommé " + str(fichier) + " a été sauté puisque le programme n'a pas pu trouver d'adresse courriel erronée.")
+					continue
+				
+				address = address.replace('"', '').replace(' ', '')
+				address = address.replace('\n', '').replace('\r', '')
+
+				bad_addresses.append(address)
+			else:
+				continue
+		
+		with open('Addresses_erronees.txt', 'w') as file:
+			to_write = ""
+			for address in bad_addresses:
+					to_write += address
+					if '\n' not in address and '\r' not in address:
+						to_write += '\n'
+			file.write(to_write)
+		messagebox.showinfo(title="Terminé", message="Tous les fichiers ont été parcourus. Un fichier nommé 'Adresses erronées' a été créé dans le dossier où se trouve le programme.")
+
+
+if __name__ == '__main__':
+	root = Tk()
+	erreurMail = ErreurMail(root)
+	root.geometry("500x200")
+	root.mainloop()
